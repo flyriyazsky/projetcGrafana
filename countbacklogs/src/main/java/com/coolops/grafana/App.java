7ef35f9d5163689649c588c0ab54bfe53a914a4e
flyriyazsky <khanriyazakhtar30@gmail.com>
2017-08-20 00:50:26 +0530

    Added new welcome COOL-1
    fixed COOL-1

--- countbacklogs/src/main/java/com/coolops/grafana/App.java
+++ countbacklogs/src/main/java/com/coolops/grafana/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Welcome to Coolops.com!" );
+        System.out.println( "Welcome to Coolops.com " );
     }
 }
